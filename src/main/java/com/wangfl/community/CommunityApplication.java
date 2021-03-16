1336c69f7f1f20d5d05205479e22f8ea5188d9ec
wfl <54830005+wangfl19@users.noreply.github.com>
2021-03-16 16:38:26 +0800

    Update CommunityApplication.java

--- src/main/java/com/wangfl/community/CommunityApplication.java
+++ src/main/java/com/wangfl/community/CommunityApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommunityApplication {
+	
+@PostConstruct
+    public void init() {
+        // 解决netty启动冲突问题
+        // see Netty4Utils.setAvailableProcessors()
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
